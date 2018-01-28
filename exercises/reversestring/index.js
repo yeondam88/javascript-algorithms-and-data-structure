f5b0d93682a4c6b2ee3b8ca44fab7b32a6778a52
Yeondam Park <giyen9088@gmail.com>
2018-01-27 21:16:21 -0800

    add more solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,8 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+/* Easiest way to solve problem */
+
 function reverse(str) {
   return str
     .split("") // turn str argument into an Array
@@ -13,4 +15,45 @@ function reverse(str) {
     .join(""); // join the array element into a string
 }
 
+/* Custom reverse function solution */
+// function reverse(str) {
+//   return str
+//     .split("") // turn str argument into an Array
+//     .reverseArray() // reverse str element
+//     .join(""); // join the array element into a string
+// }
+
+/* Solution #3 */
+
+// function reverse(str) {
+//   let reversed = "";
+
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+
+//   return reversed;
+// }
+
+/* Solution #4 */
+
+// function reverse(str) {
+//   return str
+//     .split("")
+//     .reduce((reversed, character) => character + reversed, "");
+// }
+
 module.exports = reverse;
+
+/**
+ * Creating own reverse function and added to Array prototype
+ * */
+Array.prototype.reverseArray = function() {
+  let self = this;
+  let temp = [];
+  for (let i = self.length - 1; i >= 0; i--) {
+    temp.push(self[i]);
+  }
+
+  return temp;
+};
