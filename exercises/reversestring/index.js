f3173cd499d86bb251bf24b26f89441948c97388
Yeondam Park <giyen9088@gmail.com>
2018-01-27 20:52:19 -0800

    reversestring solved

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,11 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str
+    .split("") // turn str argument into an Array
+    .reverse() // reverse str element
+    .join(""); // join the array element into a string
+}
 
 module.exports = reverse;
